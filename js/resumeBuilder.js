18b86f5a7f2f2a02a3e0ebd33234ef11013c3599
Yoav Hagashi <YoavH@top4.com>
2017-05-04 14:33:50 +0300

    update js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+ */ 
+ 
+ var name = "Yoav Hagashi";
+ var formattedName = HTMLheaderName.replace("%data%",name);
+ 
+ var role = "Server developer";
+ var formattedRole = HTMLheaderRole.replace("%data%",role);
+ 
+ 
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+ 
+ 
+ 
+ 
