20f4635ca2016b5c603d3b081e3272134ed8aca7
yoavh <yoavhagashi@gmail.com>
2017-05-13 12:46:45 +0300

    clean resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,45 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+	"name": "yoav hagashi",
+	"role": "Software engineer",
+    "welcomeMessage": "I aspire to find a position where I will have the opportunity to develop a world-class software and be part of an exciting working environment.",
+    "biopic": "images/me.jpg",
+	"contacts": {
+		"mobile": "0528704175",
+		"email": "yoavhagashi@gmail.com",
+        "github": "yoavh",
+		"location": "Tel-Aviv, Israel"
+	},
+	"skills": [
+		"Problem-Solving", "Teamwork", "Adapt to a variety of diverse cultures", "Goal Orientation", "Excellent Interpersonal Skills", "Self-Learning Capabilities", "Responsible"
+	],
+};
+
+
+var tradency ={
+    "employer":"Tradency",
+    "title":"Software engineer",
+	"location":"Nes-Ziona",
+    "dates":"November 2014 - February 2017",
+	"description":"Develop distributed windows services with multi-threaded design while emphasizing on real time performance.<br />Develop full RESTful API for web & mobile solutions.<br />Work with MS-SQL include writing stored procedures, functions and design table schema.<br />Develop push notification in real time for asynchronous financial operations using SignalR.<br />Introduced the development org to TDD methodologies using MS-Test and ensured it is being incorporated in the product life cycle by mentoring all team members.<br />Practicing Clean Code and SOLID principles while develop in C# .net with emphasis on quality.<br />Design and implement ElasticSearch solution for centralized logs system.<br />Design and implement real time notifications system for mobile using GCM and APNS.<br />Implement different 3rd party API’s into the system.<br />Develop WinForms apps with DevExpress.<br />Expanded my role as a software engineer by fulfilling additional responsibilities of a Scrum Master."
+};
+
+var work = {
+    "jobs":[tradency],
+}
+
+
+var benGurion = {
+	"name":"Ben-Gurion University of the Negev",
+    "location":"Beer-Sheva",
+	"degree":"B.sc",
+	"dates": "2007-2011",
+    "url":"http://in.bgu.ac.il/Pages/default.aspx",
+	"major":["Computer Science"],
+};
+
+var education = {
+    "schools":[benGurion]
+}
